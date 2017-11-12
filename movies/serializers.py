fa81cfdf1334fad47d494eba4bc65b6c72923459
psod18 <pavel.konstantinov.90@gmail.com>
2017-11-12 16:09:37 +0100

    Create serializers. Add urls and views

--- movies/serializers.py
+++ movies/serializers.py
@@ -1,14 +1,29 @@
-from .models import Movie, Person
+from .models import (
+    Movie,
+    Person,
+    Role,
+)
 from rest_framework import serializers
-
-
-class MovieSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Movie
-        fields = "__all__"
 
 
 class PersonSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Person
         fields = ('name', )
+
+
+class MovieSerializer(serializers.HyperlinkedModelSerializer):
+    director = PersonSerializer(read_only=False)
+    actors = PersonSerializer(read_only=False, many=True)
+
+    class Meta:
+        model = Movie
+        fields = ['id', 'title', 'description', 'director', 'actors', 'year']
+
+
+class RoleSerializer(serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+        model = Role
+        fields = '__all__'
+
--- movies/urls.py
+++ movies/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.MoviesView.as_view(), name='show_movies')
-
+    url(r'^$', views.MoviesView.as_view(), name='movies-view'),
+    url(r'^(\d+)/$', views.MovieView.as_view(), name='movie_view'),
+    url(r'^person/(\d+)/$', views.PersonView.as_view(), name='person_view'),
 ]
